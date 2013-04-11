203bbdae780b2d7dbb4a960b51f7807de0b1ab5e
Nicolás Venturo <nicolas.venturo@gmail.com>
2013-04-10 21:14:45 -0300

    Testing RF.

--- Sources/main.c
+++ Sources/main.c
@@ -10,38 +10,60 @@ void Init (void);
 
 void tx3 (void);
 void tx5 (void);
-void rx (void);
+void tx1 (void);
+void tx7 (void);
+//void rx (void);
 
-u8 mem3tx[4];
-u8 mem5tx[4];
-u8 mem3rx[4];
-u8 mem5rx[4];
-int i = 0;
+u8 mem3tx[6];
+u8 mem5tx[6];
+u8 mem1tx[4];
+u8 mem7tx[7];
 void main (void)
 {	
 	Init ();
-	mem3tx[0] = 'a';
-	mem3tx[1] = 'b';
-	mem3tx[2] = 'c';
-	mem3tx[3] = '\0';
+	mem1tx[0] = 's';
+	mem1tx[1] = 'o';
+	mem1tx[2] = 's';
+	mem1tx[3] = ' ';
 	
-	mem5tx[0] = 'x';
-	mem5tx[1] = 'y';
-	mem5tx[2] = 'z';
-	mem5tx[3] = '\0';
+	mem3tx[0] = 'm';
+	mem3tx[1] = 'e';
+	mem3tx[2] = 'd';
+	mem3tx[3] = 'i';
+	mem3tx[4] = 'o';
+	mem3tx[5] = ' ';
 	
-	rfrx_Register(3, rx, mem3rx);
-	rfrx_Register(5, rx, mem5rx);
+	mem5tx[0] = 't';
+	mem5tx[1] = 'r';
+	mem5tx[2] = 'o';
+	mem5tx[3] = 'l';
+	mem5tx[4] = 'o';
+	mem5tx[5] = ' ';
 	
-	rftx_Send(3, mem3tx, 31, NULL);
-	rftx_Send(5, mem5tx, 31, NULL);
+	mem7tx[0] = 'j';
+	mem7tx[1] = 'u';
+	mem7tx[2] = 'a';
+	mem7tx[3] = 'n';
+	mem7tx[4] = 'i';
+	mem7tx[5] = '.';
+	mem7tx[6] = '\n';
+	
+	
+//	rfrx_Register(3, rx, mem3rx);
+//	rfrx_Register(5, rx, mem5rx);
+	
+	rftx_Send(1, mem1tx, 31, tx1);
+	rftx_Send(3, mem3tx, 47, tx3);
+	rftx_Send(5, mem5tx, 47, tx5);
+
+	rftx_Send(7, mem7tx, 55, tx7);
 
 	while (1)
 	{
-		if (i == 2)
+/*		if (i == 2)
 		{
 			printf("llego %s y %s.\n",mem3rx,mem5rx);
-		}
+		}*/
 	}
 }
 
@@ -54,13 +76,32 @@ void Init (void)
 	asm cli;
 
 	// Modules that do require interrupts to be enabled
-	rfrx_Init();
+//	rfrx_Init();
 	rftx_Init(_TRUE);
 	
 	return;
 }
-
+/*
 void rx(void)
 {
 	i++;
+}*/
+
+void tx3(void)
+{
+	rftx_Send(3,mem3tx,47,tx3);
+}
+
+void tx5(void)
+{
+	rftx_Send(5,mem5tx,47,tx5);
+}
+void tx1(void)
+{
+	rftx_Send(1,mem1tx,31,tx1);
+}
+
+void tx7(void)
+{
+	rftx_Send(7,mem7tx,55,tx7);
 }
