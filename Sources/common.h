3ef6de02789038968e1b6408d39b2576a74f1fcb
Nicolás Venturo <nicolas.venturo@gmail.com>
2013-04-23 22:26:26 -0300

    Update common.h

--- Sources/common.h
+++ Sources/common.h
@@ -1,8 +1,6 @@
 #ifndef _COMMON_H
 #define _COMMON_H
 
-#include <limits.h>
-
 #define BUS_CLOCK_MHZ 50
 
 // Typedefs
@@ -14,11 +12,11 @@ typedef int s16;
 typedef unsigned long int u32;
 typedef long int s32;
 
-#define S16_MAX INT_MAX
-#define S16_MIN INT_MIN
+#define S16_MAX ((s16) 0x7FFF)
+#define S16_MIN ((s16) 0x8000)
 
-#define S32_MAX LONG_MAX
-#define S32_MIN (((s32)(-1))<<31)
+#define S32_MAX ((s32) 0x7FFFFFFF)
+#define S32_MIN (((s32)0x80000000)
 
 typedef char bool;
 #define _FALSE 0
