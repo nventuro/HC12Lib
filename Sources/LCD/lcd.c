10b7239697ea1b651f5b00c508fab8dffdd992a9
Juan Ubeira <juan.iu@hotmail.com>
2013-06-07 17:22:18 -0300

    LCD

--- Sources/LCD/lcd.c
+++ Sources/LCD/lcd.c
@@ -1,5 +1,6 @@
 #include "lcd.h"
 #include "timers.h"
+#include "error.h"
 
 #define LCD_RS_INSTR 0
 #define LCD_RS_DATA 1
@@ -38,23 +39,23 @@
 
 #define LCD_WRITE_DDRAM BIT(7)
 
-/*
-#define LCD_ENABLE_NOPS 10
-#define LCD_ENABLE_STROBE() do{u8 i;LCD_ENABLE = 1;	for (i = 0; i < LCD_ENABLE_NOPS; i++) asm nop; LCD_ENABLE = 0;} while (0)
-*/
-#define LCD_ENABLE_STROBE() do{LCD_ENABLE = 1;LCD_ENABLE = 0;} while (0)
+// Some displays work with a very short enable pulse. For those, these two lines are commented out.
+//#define LCD_ENABLE_NOPS 150
+//#define LCD_ENABLE_STROBE() do{u8 i;LCD_ENABLE = 1; for (i = 0; i < LCD_ENABLE_NOPS; i++) asm nop; LCD_ENABLE = 0;} while (0)
 
-#define LCD_MEMORY 80
+// If a delay is required when strobing enable, comment out this line.
+#define LCD_ENABLE_STROBE() do{LCD_ENABLE = 1;LCD_ENABLE = 0;} while (0)
 
 #define LCD_SHORT_DELAY_US 200
 #define LCD_LONG_DELAY_US 3200
 
+char lcd_memory[LCD_MEMORY];
+
 struct
 {
-	lcd_type type;
-	u8 index;
-	char memory[LCD_MEMORY];
-	u8 initStage;
+lcd_type type;
+u8 index;
+u8 initStage;
 } lcd_data;
 
 bool lcd_isInit = _FALSE;
@@ -64,211 +65,243 @@ void lcd_PrintCallback (void);
 
 void lcd_Init(lcd_type type)
 {
-	u32 i;
-	
-	if (lcd_isInit == _TRUE)
-		return;
-	
-	// Set pins as output
-	LCD_DATA_DDR = DDR_OUT;
-	LCD_ENABLE_DDR = DDR_OUT;
-	LCD_RS_DDR = DDR_OUT;
-	LCD_ENABLE = 0;
-	
-	
-	// Initialize the driver's memory
-	lcd_data.type = type;
-	for (i = 0; i < LCD_MEMORY; i++)
-			lcd_data.memory[i] = ' ';
-	
-	lcd_data.index = 0;
+u32 i;
 
-	// Device initialization
-	lcd_data.initStage = 0;
+if (lcd_isInit == _TRUE)
+return;
 
-	tim_Init();
-	tim_GetTimer(TIM_OC, lcd_InitCallback, NULL, LCD_TIMER);
+// Set pins as output
+LCD_DATA_DDR = DDR_OUT;
+LCD_ENABLE_DDR = DDR_OUT;
+LCD_RS_DDR = DDR_OUT;
+LCD_ENABLE = 0;
 
-	tim_SetValue (LCD_TIMER, tim_GetGlobalValue() + TIM_US_TO_TICKS(80000)); // Wait for about 80ms
-	
-	tim_ClearFlag(LCD_TIMER);
-	tim_EnableInterrupts (LCD_TIMER);
 
-	// Wait for initialization to end
-	while (lcd_isInit != _TRUE)
-		;
-		
-	return;
+// Initialize the driver's memory
+lcd_data.type = type;
+for (i = 0; i < LCD_MEMORY; i++)
+lcd_memory[i] = ' ';
+
+lcd_data.index = 0;
+
+// Device initialization
+lcd_data.initStage = 0;
+
+tim_Init();
+tim_GetTimer(TIM_OC, lcd_InitCallback, NULL, LCD_TIMER);
+
+tim_SetValue (LCD_TIMER, tim_GetGlobalValue() + TIM_US_TO_TICKS(80000)); // Wait for about 80ms
+
+tim_ClearFlag(LCD_TIMER);
+tim_EnableInterrupts (LCD_TIMER);
+
+// Wait for initialization to end
+while (lcd_isInit != _TRUE)
+;
+
+return;
 
 }
 
 void lcd_Print (char* string)
 {
-	u8 i = 0;	
-	
-	if (lcd_data.type == LCD_1602)
-	{
-		while ((string[i] != '\0') && (i < 32))
-		{
-			lcd_data.memory[i] = string[i];
-			i++;
-		}
-		while (i < 32)
-			lcd_data.memory[i++] = ' ';
-	}
-	else if (lcd_data.type == LCD_2004)
-	{
-		while ((string[i] != '\0') && (i < 80))
-		{
-			lcd_data.memory[i] = string[i];
-			i++;
-		}
-		while (i < 80)
-			lcd_data.memory[i++] = ' ';
-	}
+u8 i = 0;	
+
+if (lcd_data.type == LCD_1602)
+{
+while ((string[i] != '\0') && (i < 32))
+{
+lcd_memory[i] = string[i];
+i++;
+}
+while (i < 32)
+lcd_memory[i++] = ' ';
+}
+else if (lcd_data.type == LCD_2004)
+{
+while ((string[i] != '\0') && (i < 80))
+{
+lcd_memory[i] = string[i];
+i++;
+}
+while (i < 80)
+lcd_memory[i++] = ' ';
+}
+}
+
+void lcd_PrintRow (char* string, u8 row)
+{
+u8 i = 0;	
+
+if (lcd_data.type == LCD_1602)
+{
+if (row >= 2)
+err_Throw("lcd: attempt to access an invalid row.\n");
+
+while ((string[i] != '\0') && (i < 16))
+{
+lcd_memory[i+row*16] = string[i];
+i++;
+}
+while (i < 16)
+lcd_memory[i++ + row*16] = ' ';
+}
+else if (lcd_data.type == LCD_2004)
+{
+if (row >= 4)
+err_Throw("lcd: attempt to access an invalid row.\n");
+
+while ((string[i] != '\0') && (i < 20))
+{
+lcd_memory[i+row*20] = string[i];
+i++;
+}
+while (i < 20)
+lcd_memory[i++ + row*20] = ' ';
+}
 }
 
 void lcd_PrintCallback (void)
 {
-	LCD_RS = LCD_RS_DATA;
-	LCD_DATA = lcd_data.memory[lcd_data.index++];
-	LCD_ENABLE_STROBE();
-		
-	if ((lcd_data.index == 16) && (lcd_data.type == LCD_1602))
-	{
-		LCD_RS = LCD_RS_INSTR;
-		LCD_DATA = (LCD_WRITE_DDRAM | 0x40);
-		LCD_ENABLE_STROBE();
-	}
-	else if ((lcd_data.index == 32) && (lcd_data.type == LCD_1602))
-	{
-		LCD_RS = LCD_RS_INSTR;
-		LCD_DATA = (LCD_WRITE_DDRAM | 0x00);
-		LCD_ENABLE_STROBE();
+LCD_RS = LCD_RS_DATA;
+LCD_DATA = lcd_memory[lcd_data.index++];
+LCD_ENABLE_STROBE();
 
-		lcd_data.index = 0;
-	}
-	else if ((lcd_data.index == 20) && (lcd_data.type == LCD_2004))
-	{
-		lcd_data.index = 40;
-	}
-	else if ((lcd_data.index == 40) && (lcd_data.type == LCD_2004))
-	{
-		lcd_data.index = 60;
-	}
-	else if ((lcd_data.index == 60) && (lcd_data.type == LCD_2004))
-	{
-		lcd_data.index = 20;
-	}
-	else if ((lcd_data.index == 80) && (lcd_data.type == LCD_2004))
-	{
-		lcd_data.index = 0;
-	}
-	
-	tim_SetValue(LCD_TIMER, tim_GetValue(LCD_TIMER) + TIM_US_TO_TICKS(LCD_SHORT_DELAY_US));
+if ((lcd_data.index == 16) && (lcd_data.type == LCD_1602))
+{
+LCD_RS = LCD_RS_INSTR;
+LCD_DATA = (LCD_WRITE_DDRAM | 0x40);
+LCD_ENABLE_STROBE();
+}
+else if ((lcd_data.index == 32) && (lcd_data.type == LCD_1602))
+{
+LCD_RS = LCD_RS_INSTR;
+LCD_DATA = (LCD_WRITE_DDRAM | 0x00);
+LCD_ENABLE_STROBE();
+
+lcd_data.index = 0;
+}
+else if ((lcd_data.index == 20) && (lcd_data.type == LCD_2004))
+{
+lcd_data.index = 40;
+}
+else if ((lcd_data.index == 40) && (lcd_data.type == LCD_2004))
+{
+lcd_data.index = 60;
+}
+else if ((lcd_data.index == 60) && (lcd_data.type == LCD_2004))
+{
+lcd_data.index = 20;
+}
+else if ((lcd_data.index == 80) && (lcd_data.type == LCD_2004))
+{
+lcd_data.index = 0;
+}
+
+tim_SetValue(LCD_TIMER, tim_GetValue(LCD_TIMER) + TIM_US_TO_TICKS(LCD_SHORT_DELAY_US));
 }
 
 
 void lcd_InitCallback (void)
 {
-	switch (lcd_data.initStage)
-	{
-		case 0:
-			LCD_RS = LCD_RS_INSTR;
-			LCD_DATA = (LCD_FUNC_SET | LCD_8BIT);
-			LCD_ENABLE_STROBE();
-			
-			lcd_data.initStage = 1;
-			
-			tim_SetValue (LCD_TIMER, tim_GetValue(LCD_TIMER) + TIM_US_TO_TICKS(6400)); // Wait for about 6.4ms
-			
-			break;
-			
-		case 1:
-			LCD_RS = LCD_RS_INSTR;
-			LCD_DATA = (LCD_FUNC_SET | LCD_8BIT);
-			LCD_ENABLE_STROBE();
-			
-			lcd_data.initStage = 2;
-			
-			tim_SetValue (LCD_TIMER, tim_GetValue(LCD_TIMER) + TIM_US_TO_TICKS(320)); // Wait for about 320us
-			
-			break;
-		case 2:
-			LCD_RS = LCD_RS_INSTR;
-			LCD_DATA = (LCD_FUNC_SET | LCD_8BIT);
-			LCD_ENABLE_STROBE();
-			
-			lcd_data.initStage = 3;
-			
-			tim_SetValue (LCD_TIMER, tim_GetValue(LCD_TIMER) + TIM_US_TO_TICKS(LCD_SHORT_DELAY_US));
-			
-			break;
-		case 3:
-			// 8 bit mode stablished.
-			LCD_RS = LCD_RS_INSTR;
-			LCD_DATA = (LCD_FUNC_SET | LCD_8BIT | LCD_2LINE | LCD_SMALL_FONT);
-			LCD_ENABLE_STROBE();
-			
-			lcd_data.initStage = 4;
+switch (lcd_data.initStage)
+{
+case 0:
+LCD_RS = LCD_RS_INSTR;
+LCD_DATA = (LCD_FUNC_SET | LCD_8BIT);
+LCD_ENABLE_STROBE();
 
-			tim_SetValue (LCD_TIMER, tim_GetValue(LCD_TIMER) + TIM_US_TO_TICKS(LCD_SHORT_DELAY_US));
-			
-			break;
-		case 4:
-			LCD_RS = LCD_RS_INSTR;
-			LCD_DATA = (LCD_DISP_CTL | LCD_DISP_OFF);
-			LCD_ENABLE_STROBE();
-			
-			lcd_data.initStage = 5;
-			
-			tim_SetValue (LCD_TIMER, tim_GetValue(LCD_TIMER) + TIM_US_TO_TICKS(LCD_SHORT_DELAY_US));
-			
-			break;
-			
-		case 5:
-			LCD_RS = LCD_RS_INSTR;
-			LCD_DATA = LCD_CLEAR_DISP;
-			LCD_ENABLE_STROBE();
-			
-			lcd_data.initStage = 6;
+lcd_data.initStage = 1;
 
-			tim_SetValue (LCD_TIMER, tim_GetValue(LCD_TIMER) + TIM_US_TO_TICKS(LCD_LONG_DELAY_US));
+tim_SetValue (LCD_TIMER, tim_GetValue(LCD_TIMER) + TIM_US_TO_TICKS(6400)); // Wait for about 6.4ms
 
-			break;
-			
-		case 6:
-			LCD_RS = LCD_RS_INSTR;
-			LCD_DATA = (LCD_ENTRY_MODE | LCD_MOVE_DIR_UP | LCD_NO_SHIFT);
-			LCD_ENABLE_STROBE();
-			
-			lcd_data.initStage = 7;
-			
-			tim_SetValue (LCD_TIMER, tim_GetValue(LCD_TIMER) + TIM_US_TO_TICKS(LCD_SHORT_DELAY_US));
-			
-			break;
-			
-		case 7:
-			LCD_RS = LCD_RS_INSTR;
-			LCD_DATA = (LCD_DISP_CTL | LCD_DISP_ON | LCD_CURSOR_ON | LCD_BLINK_ON);
-			LCD_ENABLE_STROBE();
-			
-			lcd_data.initStage = 8;
-			
-			tim_SetValue (LCD_TIMER, tim_GetValue(LCD_TIMER) + TIM_US_TO_TICKS(LCD_SHORT_DELAY_US));
-			
-			break;
-			
-		case 8:
-			tim_FreeTimer(LCD_TIMER);
-			tim_GetTimer (TIM_OC, lcd_PrintCallback, NULL, LCD_TIMER);
-			tim_SetValue (LCD_TIMER, tim_GetGlobalValue() - TIM_US_TO_TICKS(LCD_SHORT_DELAY_US)); 
-			tim_ClearFlag(LCD_TIMER);
-			tim_EnableInterrupts (LCD_TIMER);
-			
-			lcd_isInit = _TRUE;
-			
-			break;
-	}
-		
+break;
+
+case 1:
+LCD_RS = LCD_RS_INSTR;
+LCD_DATA = (LCD_FUNC_SET | LCD_8BIT);
+LCD_ENABLE_STROBE();
+
+lcd_data.initStage = 2;
+
+tim_SetValue (LCD_TIMER, tim_GetValue(LCD_TIMER) + TIM_US_TO_TICKS(320)); // Wait for about 320us
+
+break;
+case 2:
+LCD_RS = LCD_RS_INSTR;
+LCD_DATA = (LCD_FUNC_SET | LCD_8BIT);
+LCD_ENABLE_STROBE();
+
+lcd_data.initStage = 3;
+
+tim_SetValue (LCD_TIMER, tim_GetValue(LCD_TIMER) + TIM_US_TO_TICKS(LCD_SHORT_DELAY_US));
+
+break;
+case 3:
+// 8 bit mode stablished.
+LCD_RS = LCD_RS_INSTR;
+LCD_DATA = (LCD_FUNC_SET | LCD_8BIT | LCD_2LINE | LCD_SMALL_FONT);
+LCD_ENABLE_STROBE();
+
+lcd_data.initStage = 4;
+
+tim_SetValue (LCD_TIMER, tim_GetValue(LCD_TIMER) + TIM_US_TO_TICKS(LCD_SHORT_DELAY_US));
+
+break;
+case 4:
+LCD_RS = LCD_RS_INSTR;
+LCD_DATA = (LCD_DISP_CTL | LCD_DISP_OFF);
+LCD_ENABLE_STROBE();
+
+lcd_data.initStage = 5;
+
+tim_SetValue (LCD_TIMER, tim_GetValue(LCD_TIMER) + TIM_US_TO_TICKS(LCD_SHORT_DELAY_US));
+
+break;
+
+case 5:
+LCD_RS = LCD_RS_INSTR;
+LCD_DATA = LCD_CLEAR_DISP;
+LCD_ENABLE_STROBE();
+
+lcd_data.initStage = 6;
+
+tim_SetValue (LCD_TIMER, tim_GetValue(LCD_TIMER) + TIM_US_TO_TICKS(LCD_LONG_DELAY_US));
+
+break;
+
+case 6:
+LCD_RS = LCD_RS_INSTR;
+LCD_DATA = (LCD_ENTRY_MODE | LCD_MOVE_DIR_UP | LCD_NO_SHIFT);
+LCD_ENABLE_STROBE();
+
+lcd_data.initStage = 7;
+
+tim_SetValue (LCD_TIMER, tim_GetValue(LCD_TIMER) + TIM_US_TO_TICKS(LCD_SHORT_DELAY_US));
+
+break;
+
+case 7:
+LCD_RS = LCD_RS_INSTR;
+LCD_DATA = (LCD_DISP_CTL | LCD_DISP_ON | LCD_CURSOR_ON | LCD_BLINK_ON);
+LCD_ENABLE_STROBE();
+
+lcd_data.initStage = 8;
+
+tim_SetValue (LCD_TIMER, tim_GetValue(LCD_TIMER) + TIM_US_TO_TICKS(LCD_SHORT_DELAY_US));
+
+break;
+
+case 8:
+tim_FreeTimer(LCD_TIMER);
+tim_GetTimer (TIM_OC, lcd_PrintCallback, NULL, LCD_TIMER);
+tim_SetValue (LCD_TIMER, tim_GetGlobalValue() - TIM_US_TO_TICKS(LCD_SHORT_DELAY_US));
+tim_ClearFlag(LCD_TIMER);
+tim_EnableInterrupts (LCD_TIMER);
+
+lcd_isInit = _TRUE;
+
+break;
+}
+
 }
--- Sources/LCD/lcd.h
+++ Sources/LCD/lcd.h
@@ -9,6 +9,12 @@ typedef enum
 	LCD_2004	// 20 characters, 4 rows
 } lcd_type;
 
+#define LCD_1602_COLS 16
+#define LCD_1602_ROWS 2
+
+#define LCD_2004_COLS 20
+#define LCD_2004_ROWS 4
+
 #define LCD_DATA PORTB // The 8-bit port used to send data to the device.
 #define LCD_DATA_DDR DDRB // The DDR register for LCD_DATA
 #define LCD_ENABLE PTH_PTH6 // The enable (clock) pin.
@@ -23,15 +29,31 @@ typedef enum
 #define LCD_TIMER 7 // The timer module used for timing by the LCD module. This pin should be left unconnected.
 
 
+#define LCD_MEMORY (LCD_2004_ROWS * LCD_2004_COLS)
+extern char lcd_memory[LCD_MEMORY];
+// After calling lcd_Init(), the LCD's screen will match what's on lcd_memory. A new row commences every (lcd_type)_COLS characters.
+// If the memory can hold more characters than the LCD can (because it's of a smaller size), only the first characters are used.
+
+
 void lcd_Init(lcd_type type);
 // Initializes the LCD module. This requires the Timers module to work, and interrupts to be enabled.
-// type determines the number of rows and columns the module expects the device to have. A wrong value on type will 
+// type determines the number of rows and columns the module expects the device to have. A wrong value on type will
 // cause faulty behavior. After lcd_Init is called, type cannot be changed.
 
 void lcd_Print (char* string);
-// Prints a string on the device, starting on the first row and first column. 
+// Prints a string on the device, starting on the first row and first column.
 // If the string is not null-terminated, or too long, the module will only print as many characters as the device is capable of displaying.
 // If the string has less characters than the device can display, the remaining characters are left empty.
 // This deletes any message previously printed on the device.
+// Calling lcd_Print is the same as copying string in lcd_memory, filling the rest of it with whitespaces.
+
+void lcd_PrintRow (char* string, u8 row);
+// Prints a string on the device, starting on the first column of the row row. The first row is row 0.
+// If the string is not null-terminated, or too long, the module will only print as many characters as the device is capable of displaying.
+// Only the contents of the selected row will be modified.
+// If the string has less characters than the device can display, the remaining characters are left empty.
+// This deletes any message previously printed on the device (on that row).
+// Calling lcd_Print is the same as copying string in lcd_memory, on the positions corresponding to the
+// selected row, filling the rest of it with whitespaces.
 
 #endif
