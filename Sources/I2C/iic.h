408dd344c1a957a13ae4a09e6749299dc40ebe72
Nicolás Venturo <nicolas.venturo@gmail.com>
2013-07-01 03:46:22 -0300

    Modified the comments style in iic.h

--- Sources/I2C/iic.h
+++ Sources/I2C/iic.h
@@ -33,40 +33,40 @@ extern iic_commData_T iic_commData;
 
 
 void iic_Init (void);
-/*	Inits I2C module. Must be called at the beginning of the program.
-	Baud rate is hard-coded in IIC_SET_BAUD() macro (see iic.c).
-*/
+// 	Initializes the IIC module. This requires no other modules to work, and doesn't require interrupts to be enabled.
+//	Baud rate is hard-coded in IIC_SET_BAUD() macro (see iic.c).
+
 
 void iic_Send (u8 slaveAddress, iic_ptr eotCB, iic_ptr commFailedCB, u8 toWrite, u8* sendBuffer);
-/*	Sends data to the device specified in slaveAddress, calling eotCB when finished or commFailedCB 
-	if it fails for	any reason.
-	@param 'toWrite' is the size of the package to send.
-	If 'sendBuffer' is not NULL, data is sent from that point in memory up to 'toWrite' bytes.
-	If 'sendBuffer' is NULL, all the data to send must be written in iic_commData.data.
-	Note: slaveAddress is composed by 7 bits, aligned RIGHT (X as MSB).
-	The first byte to send should be write address inside slave device.
-*/
+//	Sends data to the device specified in slaveAddress, calling eotCB when finished or commFailedCB 
+//	if it fails for	any reason.
+//	@param 'toWrite' is the size of the package to send.
+//	If 'sendBuffer' is not NULL, data is sent from that point in memory up to 'toWrite' bytes.
+//	If 'sendBuffer' is NULL, all the data to send must be written in iic_commData.data.
+//	Note: slaveAddress is composed by 7 bits, aligned RIGHT (X as MSB).
+//	The first byte to send should be write address inside slave device.
+
 
 
 void iic_Receive (u8 slaveAddress, iic_ptr eotCB, iic_ptr commFailedCB, u8 toRead, u8* receiveBuffer);
-/*	Receives data from the device specified in slaveAddress, calling eotCB when finished or commFailedCB 
-	if it fails for	any reason.
-	Slave device has to know where to start reading before calling iic_receive, so proper register address 
-	must be sent before receiving.
-	
-	The size of the package to receive is specified in 'toRead', and the result is stored in 
-	'receiveBuffer' (the user must provide enough memory). If 'receiveBuffer' is NULL, the result is
-	stored in iic_commData.data, and the package size must be less or equal to IIC_MEM_SIZE.
-	Note: slaveAddress is composed by 7 bits, aligned RIGHT (X as MSB).
-*/
+//	Receives data from the device specified in slaveAddress, calling eotCB when finished or commFailedCB 
+//	if it fails for	any reason.
+//	Slave device has to know where to start reading before calling iic_receive, so proper register address 
+//	must be sent before receiving.
+//	
+//	The size of the package to receive is specified in 'toRead', and the result is stored in 
+//	'receiveBuffer' (the user must provide enough memory). If 'receiveBuffer' is NULL, the result is
+//	stored in iic_commData.data, and the package size must be less or equal to IIC_MEM_SIZE.
+//	Note: slaveAddress is composed by 7 bits, aligned RIGHT (X as MSB).
+
 
 void iic_ReceiveFromRegister (u8 regAddress, u8 slaveAddress, iic_ptr eotCB, iic_ptr commFailedCB, u8 toRead, u8* receiveBuffer);
-/*	Complete reception from slave device and specified register. Uses iic_Receive. 
-	Note: slaveAddress is composed by 7 bits, aligned RIGHT (X as MSB).
-*/
+//	Complete reception from slave device and specified register. Uses iic_Receive. 
+//	Note: slaveAddress is composed by 7 bits, aligned RIGHT (X as MSB).
+
 
 #define iic_IsBusy() ((IIC0_IBSR_IBB == 1) ? (_TRUE) : (_FALSE) )
-/*	Checks the bus' status. */
+//	Checks the bus' status.
 
 
 void iic_FlushBuffer(void);
