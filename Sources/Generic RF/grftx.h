f5597e8c9b9b014aa3a5cfa733fbccc6d0929aad
Nicolás Venturo <nicolas.venturo@gmail.com>
2013-07-01 16:47:49 -0300

    Fixed a comment in grftx.

--- Sources/Generic RF/grftx.h
+++ Sources/Generic RF/grftx.h
@@ -43,7 +43,7 @@ void grftx_Init(bool ecc);
 void grftx_Send(u8 id, u8 *data, u8 length, grftx_ptr eot);
 // If the transmitter is idle, this commences data transmission. Otherwise, the transmission request is stored in a FIFO queue and will be 
 // completed at a later time. In both of these scenarios, grftx_Send returns _TRUE. However, if the FIFO queue is full, the request is not stored,
-// and grftx_Send returns _FALSE. There are as many queue slots as there are id's.
+// and an error is thrown. There are as many queue slots as there are id's.
 // id indicates the id of the receiver (from 0 to 7). The content of the message (of length+1 bits, up to 128, from MSB TO LSB) must be stored beforehand in data, and 
 // must not be modified until eot is called, since the module will be actively using this portion of RAM.
 // eot is called when the tranmission ends, and a new tranmission can be requested at this time.
